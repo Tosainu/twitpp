9f5af3c881b0d7d15c4237e5b6b3322c011fe87d
Tosainu <tosainu.maple@gmail.com>
2015-03-16 15:07:12 +0900

    oops

--- twitpp/twitpp.hh
+++ twitpp/twitpp.hh
@@ -1,7 +1,7 @@
 #if !defined TWITPP_HH
 #define      TWITPP_HH
 
-#include <twitpp/oauth/oauth.h>
-#include <twitpp/oauth/account.h>
+#include <twitpp/oauth/oauth.hh>
+#include <twitpp/oauth/account.hh>
 
 #endif // TWITPP_HH
